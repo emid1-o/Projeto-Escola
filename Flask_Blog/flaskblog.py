b16ff6774e781fdb89f3602ebaa30e19b0ca2de8
emid1-o <emidiolop5@gmail.com>
2025-09-10 14:34:39 -0300

    layout / home / about

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+
+        {
+            'author': 'Corey Schafer',
+            'title': 'Jaws',
+            'content': 'first post content',
+            'date_posted': 'April 20, 2018'
+        },
+        {
+            'author': 'Emidio Neto',
+            'title': 'Jaws 3',
+            'content': 'Second post content',
+            'date_posted': 'April 18, 2018'
+        },
+
+]
+
+
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about_page():
-    return "<p>This is my about page blablabla</p>"
+    return render_template('about.html', title = 'About')
 
 if __name__ == '__main__':
     app.run(debug=True)
