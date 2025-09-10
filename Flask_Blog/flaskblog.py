44bd53075e34660011aed48bba3cad4843db7cdc
emid1-o <emidiolop5@gmail.com>
2025-09-10 15:50:15 -0300

    register dummy data completo funcionando

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0a6666ca7be09ffc36e16fe5ed2ce898'
 
 posts = [
 
@@ -21,12 +24,26 @@ posts = [
 
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about_page():
     return render_template('about.html', title = 'About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Acoount created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
