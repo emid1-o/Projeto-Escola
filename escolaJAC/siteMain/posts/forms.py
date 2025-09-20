5a9b265d933e8a88bc2c54884059ceda9716b170
emid1-o <emidiolop5@gmail.com>
2025-09-20 02:01:59 -0300

    fix: mensagens do validator em portugues

--- escolaJAC/siteMain/posts/forms.py
+++ escolaJAC/siteMain/posts/forms.py
@@ -4,8 +4,8 @@ from wtforms import StringField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
-    title = StringField('Título', validators=[DataRequired()])
-    content = TextAreaField('Conteúdo', validators=[DataRequired()])
+    title = StringField('Título', validators=[DataRequired(message='Campo obrigatório')])
+    content = TextAreaField('Conteúdo', validators=[DataRequired('Campo obrigatório')])
     submit = SubmitField('Postar')
     is_pinned = BooleanField('Fixar este anúncio?') 
-    picture = FileField('Postar Imagem', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    picture = FileField('Postar Imagem', validators=[FileAllowed(['jpg', 'png', 'jpeg'], message='Selecione apenas arquivos suportados')])
